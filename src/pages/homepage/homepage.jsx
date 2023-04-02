3067d1ba9078d7cc7d8f331d5b59efec3965ee36
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-04-02 02:13:16 +0100

    css modifications

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,12 +2,15 @@ import BookOrder from "../../components/book-order/bookorder.component";
 import Introduction from "../../components/introduction/introduction.component";
 import AboutSection from "../../components/aboutSection/aboutsecton.component";
 import SampleBlog from "../../components/sample-blogs/samples.component";
-
+import {useEffect} from 'react';
 
 import './homepage.css';
 
 
 const HomePage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, []);
     return (
         <div className="homepage">
             <Introduction />
